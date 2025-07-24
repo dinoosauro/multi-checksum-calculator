2842a2fa69b354a105685772bdb59ba90cb1bdde
dinoosauro <80783030+dinoosauro@users.noreply.github.com>
2025-07-24 22:13:44 +0200

    Added custom file name input & downloaded file alert - The user can now customize the output CSV filename thanks to an input    * the `.csv` extension is automatically added if the user doesn't write it - Now an alert appears at the top of the screen when the user downloads the CSV. It'll stay visible for 5 seconds, and it'll also have an anchor element to redownload the CSV file - Bumped version to 1.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -83,6 +83,7 @@ export default function App() {
   const [progress, updateProgress] = useState<OutputProgress[]>([]); // The files that are being handled, or that have already been handled
   const [showTables, updateShowTables] = useState(false); // Show the "Progress" and "Result" cards (so, this must be triggered after the user has chosen a file)
   const [showLicenses, updateShowLicenses] = useState(false); // Show the "Open source licenses" dialog
+  const [csvDownloadLink, updateCsvDownloadLink] = useState<string | null>(null); // If this value is a string, an alert will be displayed at the top so that the user can redownload the csv file.
   /**
    * The list of available algorithms as a key, and as a value if they should be used for this conversion. 
    */
@@ -96,11 +97,15 @@ export default function App() {
     MD5: false,
     RIPEMD160: false,
   });
+  /**
+   * The file name the user has chosen for the CSV file
+   */
+  let csvFileName = useRef("");
   if (!conversionSettings.current.hasSettingsBeenRestored) { // Restore the settings. We cannot use an Effect since they're stored in a Ref and not in a State, so the default values of the inputs would be different 
     conversionSettings.current.hasSettingsBeenRestored = true;
     const prevSettings = JSON.parse(localStorage.getItem("MultiChecksumCalc-Settings") ?? "{}");
     for (const key in prevSettings) {
-      if (typeof prevSettings[key] === "number" || typeof prevSettings[key] === "boolean") conversionSettings.current[key as "pickFolder"] = prevSettings[key] as boolean;
+      if (typeof prevSettings[key] === "number" || typeof prevSettings[key] === "boolean" && key !== "currentOperations") conversionSettings.current[key as "pickFolder"] = prevSettings[key] as boolean;
     }
     const prevAlgo = JSON.parse(localStorage.getItem("MultiChecksumCalc-AlgoStorage") ?? "{}");
     for (const key in prevAlgo) {
@@ -152,7 +157,7 @@ export default function App() {
       max: file.size,
       id: CryptoJS.lib.WordArray.random(16).toString(CryptoJS.enc.Hex)
     }
-    conversionSettings.current.trackProgress && updateProgress(prev => [...prev, currentProgress]); 
+    conversionSettings.current.trackProgress && updateProgress(prev => [...prev, currentProgress]);
     for (let offset = 0; offset < file.size; offset += conversionSettings.current.chunkSize) { // Let's get this chunk of the Blob, and continue handling the hash.
       const chunk = file.slice(offset, offset + conversionSettings.current.chunkSize);
       const arrayBuffer = await chunk.arrayBuffer();
@@ -191,6 +196,27 @@ export default function App() {
     conversionSettings.current.trackProgress && updateProgressBar();
     getShaSum(true);
   }
+  /**
+   * The alert div that tells the user the download has started. A link to redownload the CSV is appended.
+   */
+  const downloadDialog = useRef<HTMLDivElement>(null);
+  useEffect(() => { // Show the downloader div for 5 seconds. An opacity transition is added.
+    setTimeout(() => {
+      if (downloadDialog.current) {
+        downloadDialog.current.style.opacity = "1";
+        setTimeout(() => {
+          if (downloadDialog.current) downloadDialog.current.style.opacity = "0";
+          setTimeout(() => updateCsvDownloadLink(prev => { // Maybe the link has changed, and in this case we don't need to put it as null
+            if (prev !== csvDownloadLink) {
+              if (downloadDialog.current) downloadDialog.current.style.opacity = "1"; // Let's make this item visible again, since the link is different
+              return prev;
+            }
+            return null;
+          }), 200);
+        }, 5000)
+      }
+    }, 15)
+  }, [csvDownloadLink])
   return <>
     <header>
       <div className="flex hcenter gap">
@@ -240,6 +266,7 @@ export default function App() {
             if (!input.files) return;
             updateShowTables(true); // Show the Progress and Result tables
             fileArr.push(...input.files);
+            console.log(`Added ${input.files.length} files`)
             for (let i = 0; i < Math.min(conversionSettings.current.maxOperations, input.files.length); i++) getShaSum();
           },
         });
@@ -265,39 +292,53 @@ export default function App() {
               )}
             </tbody>
           </table>
-        </div><br></br><br></br>
-        <button onClick={() => {
-          let str = `"File name:",`;
-          for (const item of tableColumns) str += `"${item}",`;
-          str = str.substring(0, str.length - 1);
-          for (const file of results) str += `\n"${file.fileName.replace(/\"/g, "\"\"")}",\"${file.hashes.map(item => `"${item}"`).join("\",\"")}\"`;
-          const a = Object.assign(document.createElement("a"), {
-            href: URL.createObjectURL(new Blob([str])),
-            download: `MultiChecksumCalc-${Date.now()}.csv`
-          });
-          a.click();
-        }}>Export as a CSV file</button>
-      </div><br></br>
-        <div className="card">
-          <h2>Reading progress:</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>File name:</th>
-                <th>Progress:</th>
-              </tr>
-            </thead>
-            <tbody>
-              {progress.map((singleProgress, i) => <tr key={singleProgress.id}>
-                <td>{singleProgress.fileName}</td>
-                <td><progress ref={(item) => { progress[i].progress = item ?? undefined }} max={singleProgress.max}></progress></td>
-              </tr>
-              )}
-            </tbody>
-          </table>
+        </div><br></br>
+        <div className="card" style={{ backgroundColor: "var(--secondcard)" }}>
+          <h3>Download CSV file</h3>
+          <label className="flex hcenter gap">
+            File name: <input type="text" defaultValue={csvFileName.current} onChange={(e) => { csvFileName.current = e.target.value; }}></input>
+          </label><br></br>
+          <button onClick={() => {
+            let str = `"File name:",`;
+            for (const item of tableColumns) str += `"${item}",`;
+            str = str.substring(0, str.length - 1);
+            for (const file of results) str += `\n"${file.fileName.replace(/\"/g, "\"\"")}",\"${file.hashes.map(item => `"${item}"`).join("\",\"")}\"`;
+            const trimmedFileName = csvFileName.current.trim();
+            const a = Object.assign(document.createElement("a"), {
+              href: URL.createObjectURL(new Blob([str])),
+              download: trimmedFileName === "" ? `MultiChecksumCalc-${Date.now()}.csv` : `${trimmedFileName}${trimmedFileName.endsWith(".csv") ? "" : ".csv"}`
+            });
+            a.click();
+            updateCsvDownloadLink(a.href); // Show the "File downloaded" alert at the top of the screen
+          }}>Export as a CSV file</button>
         </div>
+      </div><br></br>
+      <div className="card">
+        <h2>Reading progress:</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>File name:</th>
+              <th>Progress:</th>
+            </tr>
+          </thead>
+          <tbody>
+            {progress.map((singleProgress, i) => <tr key={singleProgress.id}>
+              <td>{singleProgress.fileName}</td>
+              <td><progress ref={(item) => { progress[i].progress = item ?? undefined }} max={singleProgress.max}></progress></td>
+            </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </>}<br></br><br></br>
     <p className="flex gap wrap">Everything is elaborated locally. <u onClick={() => updateShowLicenses(true)}>View open source licenses</u><a href="https://github.com/dinoosauro/multi-checksum-calculator" target="_blank">View on GitHub</a></p>
     {showLicenses && <OpenSourceDialog close={() => updateShowLicenses(false)}></OpenSourceDialog>}
+    {csvDownloadLink && <div className="topDialog flex hcenter gap wcenter" ref={downloadDialog}>
+      <span>Download started!</span><a href={csvDownloadLink} target="_blank" download={(() => {
+        const trimmedFileName = csvFileName.current.trim();
+        return trimmedFileName === "" ? `MultiChecksumCalc-${Date.now()}.csv` : `${trimmedFileName}${trimmedFileName.endsWith(".csv") ? "" : ".csv"}`;
+      })()}>Try again</a>
+    </div>}
   </>
 }
